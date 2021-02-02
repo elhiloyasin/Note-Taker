c146499ca600e452e4669673610cb78e9010678b
elhiloyasin <72108312+elhiloyasin@users.noreply.github.com>
2021-02-02 01:58:41 -0500

    Update notes.js

--- db/notes.js
+++ db/notes.js
@@ -5,21 +5,15 @@ const uuidv1 = require('uuid/v1');
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
-
-
 class Notes {
     readNotes(){
      return readFileAsync('db/db.json', 'utf8',)
-
-
     }
 
     writeNotes(note){
         return writeFileAsync('db/db.json', JSON.stringify(note))
-   
-   
        }
-
+    
         getNotes(){
         return this.readNotes().then(data =>{
             let addNotes;
